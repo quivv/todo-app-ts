dbe0ac9af5c5b164cd4e3254552dd33c7416f324
qui <quivv.dtp@gmail.com>
2022-06-28 06:54:26 +0700

    Qui add first

---
+++ src/components/InputFeild.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+const InputFeild = () => {
+  return <form className='input'>
+    <input type='input' placeholder='Enter a task' className='input__box'/>
+    <button className='input__submit' type='submit'>GO</button>
+  </form>
+}
+
+export default InputFeild
