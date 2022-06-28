578911c56dff83a76655f749483879f341aa51cb
qui <quivv.dtp@gmail.com>
2022-06-29 06:57:49 +0700

    QUI add 6/29/2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import Todolist from './components/Todolist';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+      setTodo("")
+    }
+  }
+
+  console.log(todo);
+
   return (
     <div className="App">
      <span className='heading'>ToDoList</span>
-     <InputFeild/>
+     <InputFeild todo={todo} setTodo={setTodo} handleAdd = {handleAdd}/>
+     <Todolist todos={todos} setTodos={setTodos}/>
+    
     </div>
   );
 }
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './style.css'
 
-const InputFeild = () => {
-  return <form className='input'>
-    <input type='input' placeholder='Enter a task' className='input__box'/>
+type Props = {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+const InputFeild: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return <form className='input' onSubmit={(e) => {
+      handleAdd(e)
+      inputRef.current?.blur();
+    }}>
+    <input
+      ref={inputRef}
+      type='input'
+      value={todo}
+      onChange={(e) => setTodo(e.target.value)}
+      placeholder='Enter a task'
+      className='input__box'
+    />
     <button className='input__submit' type='submit'>GO</button>
   </form>
 }
