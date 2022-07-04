16857aa1c06ea60d94cde1b289c281c07646636d
qui <quivv.dtp@gmail.com>
2022-07-05 04:59:26 +0700

    Qui add finished

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from '../model';
 import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
@@ -28,11 +28,23 @@ const SingleTodo: React.FC<Props> = ({todo, todos, setTodos}) => {
     setEdit(false);
   }
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // inputRef.current?.focus();
+    console.log(inputRef)
+  }, [edit])
+
   return (
     <form className='todos__single' onSubmit={(e) => handleEdit(e, todo.id)}>
       {
         edit ?(
-          <input value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className='todos__single--text'/>
+          <input 
+            ref={inputRef}
+            value={editTodo}
+            onChange={(e) => setEditTodo(e.target.value)} 
+            className='todos__single--text'
+          />
         ) : (
           todo.isDone ? (
             <s className="todos__single--text">{todo.todo}</s> 
